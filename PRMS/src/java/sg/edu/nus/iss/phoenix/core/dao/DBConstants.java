b2d13c7715d0f29482be3af6ba9e0330c9bd019a
Sarita Sethy <e0146848@u.nus.edu>
2017-09-21 23:17:27 +0800

    changes as per new schema

--- PRMS/src/java/sg/edu/nus/iss/phoenix/core/dao/DBConstants.java
+++ PRMS/src/java/sg/edu/nus/iss/phoenix/core/dao/DBConstants.java
@@ -4,9 +4,9 @@ public class DBConstants {
 	
 	// Data Connection Variables
 	public static final String COM_MYSQL_JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	public static final String dbUrl = "jdbc:mysql://localhost:3306/phoenix";
+	public static final String dbUrl = "jdbc:mysql://127.0.0.1:3306/phoenixft04";
 	public static final String dbUserName = "root";
-	public static final String dbPassword = "password";
+	public static final String dbPassword = "root";
 	 
 	//Not yet refactored!!!!!!!!!!!!!!!!!!!
 	
@@ -14,10 +14,8 @@ public class DBConstants {
     public static final String usersTableName = "user";
     public static final String rolesTableName = "role";
    
-    
-    
     //Users table field names
-    public static final String u_id = "id";
+    public static final String u_id = "userid";
     public static final String u_name = "name";
     public static final String u_password = "password";
     public static final String u_role ="role";
