1330d950478988e565e732348269ec2f0d7fb119
Sarita Sethy <e0146848@u.nus.edu>
2017-10-03 14:30:07 +0800

    Changed the file as per new Schema

--- PRMS/src/java/sg/edu/nus/iss/phoenix/core/util/PhoenixUtil.java
+++ PRMS/src/java/sg/edu/nus/iss/phoenix/core/util/PhoenixUtil.java
@@ -7,7 +7,7 @@ import java.util.Calendar;
 public class PhoenixUtil {
 	
 	public static final DateFormat dateTimeFormatter = new SimpleDateFormat("dd-MM-yyyy HH:mm");
-	public static final SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy");
+	public static final SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd");
 	private static int timeHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
 	private static int timeMinute = Calendar.getInstance().get(Calendar.MINUTE);
 	private static final DateFormat dateTimeAMPMFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
